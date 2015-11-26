28970f4aad421298286f1b4b90569c98064c0f40
Matthew Brown <matdone93@gmail.com>
2015-11-26 11:01:32 +0100

    Initial Commit

---
+++ calculator_rft/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator_rft
+//
+//  Created by Cactus Joe on 26/11/15.
+//  Copyright © 2015 Cactus Joe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
